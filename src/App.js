3ce557920cfdba6f81eff0be4ad7f0886bb7054b
kaushal77 <kaushal.chitlangia97@gmail.com>
2022-07-12 15:27:13 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} /> */}
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
